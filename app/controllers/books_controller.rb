ec655e064912aca9c1507c3ba1bb7f6a601c738b
john_sellers <jsellers5@gmu.edu>
2014-04-01 19:42:34 -0400

    intial check

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,9 @@
+class BooksController < ApplicationController
+  def index
+  	@available_at = Time.now
+	@books = Book.all
+  end
+  def show
+    @book = Book.find(params[:id])
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookstore::Application.config.secret_key_base = '525da18727fe0e627361e29a919e05c7af91ac2c398b66e80acedea9774b5496980ca2d2e44bc11bb4aaf88e21202293c032a5b9a6556837337d47ef8a57e075'
