b9e580194cd674a1abc67a12761c7a6b6f5ea920
john_sellers <jsellers5@gmu.edu>
2014-04-01 20:28:21 -0400

    create new book page

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -15,16 +15,4 @@ class BooksControllerTest < ActionController::TestCase
   def new
   @book = Book.new
 end
-
-def create
-  @book = Book.new(book_params)
-  @book.save
-  redirect_to @book
-end
-
-private
-
-def book_params
-  params.require(:book).permit(:title, :author, :pages, :price)
-end
 end
