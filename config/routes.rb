26e3e11a89992ac31c64970bf54afe64912c2e21
john_sellers <jsellers5@gmu.edu>
2014-04-01 21:24:23 -0400

    add rest routes and partials

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Bookstore::Application.routes.draw do
   get "/books/:id/edit" => "books#edit", as: 'edit_book'
   patch "/books/:id" => "books#update"
   delete "/books/:id" => "books#destroy"
+  resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
